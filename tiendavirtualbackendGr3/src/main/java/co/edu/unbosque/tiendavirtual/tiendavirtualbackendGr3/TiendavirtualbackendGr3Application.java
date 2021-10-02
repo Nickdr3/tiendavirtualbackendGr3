8246b37640d5ec8768001d2a91275381de215a21
Nickdr3 <nickdrp17@gmail.com>
2021-10-01 21:18:50 -0500

    cambio

--- tiendavirtualbackendGr3/src/main/java/co/edu/unbosque/tiendavirtual/tiendavirtualbackendGr3/TiendavirtualbackendGr3Application.java
+++ tiendavirtualbackendGr3/src/main/java/co/edu/unbosque/tiendavirtual/tiendavirtualbackendGr3/TiendavirtualbackendGr3Application.java
@@ -3,6 +3,7 @@ package co.edu.unbosque.tiendavirtual.tiendavirtualbackendGr3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Test cambio cristian
+//test
 @SpringBootApplication
 public class TiendavirtualbackendGr3Application {
 
